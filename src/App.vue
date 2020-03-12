835dfd860789bb8199064410fb973ba794e3db9a
jacobwilsonhamilton <jacobwilsonhamilton@gmail.com>
2020-03-12 14:07:11 -0500

    added stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<HelloWorld msg="Biden Insult Generator" />
+		<a href="https://hamilsauce.github.io/bidenisms/images/6.jpg">
+			<img
+				alt="dwight"
+				src="https://hamilsauce.github.io/bidenisms/images/6.jpg"
+				width="100%"
+			>
+		</a>
+		<div class="button-container">
+			<button class="new-button">New Insult</button>
+			<button class="share-button">Share</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			HelloWorld
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
